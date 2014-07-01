0838ccd34f0363dc85bea122b9db65d868ab9b5c
Claudin <claudio.berroeta@ing.uchile.cl>
2014-07-01 18:25:44 -0400

    Vecino mas cercano

--- src/cc4102/kdtree/ClosestNeighborResolver.java
+++ src/cc4102/kdtree/ClosestNeighborResolver.java
@@ -1,6 +1,7 @@
 package cc4102.kdtree;
 
 import cc4102.EuclideanTSPResolver;
+import cc4102.util.hamiltonian.HVertex;
 import cc4102.util.hamiltonian.HamiltonianCircuit;
 import cc4102.util.TSPPoint;
 
@@ -10,9 +11,33 @@ public class ClosestNeighborResolver implements EuclideanTSPResolver{
 
     @Override
     public HamiltonianCircuit resolveTSP(List<TSPPoint> points) {
-       // KDTree kdTree = KDTreeFactory.makeMedianTreeWith(points);
-        HamiltonianCircuit C = new HamiltonianCircuit(points.get(0));
 
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        List<TSPPoint> leftovers = points;
+        int rand = (int) Math.random()*(leftovers.size()-1);
+        HamiltonianCircuit hc = new HamiltonianCircuit(leftovers.get(rand));
+        leftovers.remove(rand);
+
+        while (leftovers.size()>0){
+            double min = Double.POSITIVE_INFINITY;
+            HVertex minHc= null;
+            TSPPoint p = null;
+            int indexp = -1;
+
+            for(TSPPoint cand: leftovers){
+                minHc = hc.calcMinDistance(cand);
+                double minp = cand.distance(minHc.getPoint());
+                if (minp<min){
+                    min=minp;
+                    p=cand;
+                    indexp=leftovers.indexOf(cand);
+                }
+            }
+
+            leftovers.remove(indexp);
+            hc.addPointAfter(p,minHc);
+
+        }
+
+        return hc;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
