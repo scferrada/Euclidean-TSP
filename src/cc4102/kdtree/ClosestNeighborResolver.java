8701735558f25260c55650615a2df4e9cd4a951d
Claudin <claudio.berroeta@ing.uchile.cl>
2014-07-01 17:42:08 -0400

    asd

--- src/cc4102/kdtree/ClosestNeighborResolver.java
+++ src/cc4102/kdtree/ClosestNeighborResolver.java
@@ -5,12 +5,41 @@ import cc4102.util.hamiltonian.HamiltonianCircuit;
 import cc4102.util.TSPPoint;
 import cc4102.kdtree.tree.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ClosestNeighborResolver implements EuclideanTSPResolver{
 
     @Override
     public HamiltonianCircuit resolveTSP(List<TSPPoint> points) {
+        List<TSPPoint> circuit = new LinkedList<TSPPoint>();
+        List<TSPPoint> leftovers = points;
+        int rand = (int) Math.random()*leftovers.size();
+        circuit.add(leftovers.get(rand));
+        leftovers.remove(rand);
+
+        while (leftovers.size()>0){
+            double min = Double.MAX_VALUE;
+            TSPPoint p = null;
+            int indexp = -1;
+
+            for(TSPPoint cand: leftovers){
+                double minp = calcMin(cand,circuit);
+                if (minp<min){
+                    min=minp;
+                    p=cand;
+                    indexp=leftovers.indexOf(cand);
+                }
+            }
+
+            leftovers.remove(indexp);
+            TSPPoint near =
+
+        }
+
+
+
+
         KDTree kdTree = KDTreeFactory.makeMedianTreeWith(points);
         HamiltonianCircuit C = new HamiltonianCircuit(points.get(0));
 
